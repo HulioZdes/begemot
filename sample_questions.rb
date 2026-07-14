60ace8860134e0ccc44318fe0d6c9311114e6876
Evgeniy <jmartblues@ukr.net>
2026-07-14 19:41:47 +0300

    Create sample_questions.rb

---
+++ sample_questions.rb
@@ -0,0 +1,45 @@
+require_relative "question"
+
+QUESTIONS = [
+
+  Question.new(
+    "Which language powers Ruby on Rails?",
+    [
+      "Ruby",
+      "Java",
+      "Go"
+    ],
+    1
+  ),
+
+  Question.new(
+    "Which company created Go?",
+    [
+      "Google",
+      "Apple",
+      "Oracle"
+    ],
+    1
+  ),
+
+  Question.new(
+    "Which keyword defines a class in Ruby?",
+    [
+      "class",
+      "object",
+      "struct"
+    ],
+    1
+  ),
+
+  Question.new(
+    "What extension does a Ruby file use?",
+    [
+      ".rb",
+      ".py",
+      ".go"
+    ],
+    1
+  )
+
+]
